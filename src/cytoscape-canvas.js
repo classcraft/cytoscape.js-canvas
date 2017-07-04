b86cfcad0bf4b493fd78bf54182901f21611d654
Alex Nault <nault.alex@gmail.com>
2017-07-04 16:44:01 -0400

    Add Babel & eslint

--- src/cytoscape-canvas.js
+++ src/cytoscape-canvas.js
@@ -1,75 +1,75 @@
-;(function() { 'use strict';
+(function () {
 	// registers the extension on a cytoscape lib ref
-	var register = function(cytoscape) {
+	const register = function (cytoscape) {
 		if (!cytoscape) {
 			return;
 		}
 
-		var cyCanvas = function(args) {
-			var cy = this;
-			var container = cy.container();
-			var $container = $(container);
+		const cyCanvas = function (args) {
+			const cy = this;
+			const container = cy.container();
+			const $container = $(container);
 
-			var canvas = document.createElement('canvas');
-			var $canvas = $(canvas);
+			const canvas = document.createElement("canvas");
+			const $canvas = $(canvas);
 
 			$container.append($canvas);
 
-			var ctx = $canvas[0].getContext('2d');
+			const ctx = $canvas[0].getContext("2d");
 
-			var defaults = {
+			const defaults = {
 				zIndex: 1,
 				pixelRatio: "auto",
 			};
 
-			var options = Object.assign({}, defaults, args);
+			const options = Object.assign({}, defaults, args);
 
 			if (options.pixelRatio === "auto") {
 				options.pixelRatio = window.devicePixelRatio || 1;
 			}
 
 			function resize() {
-				var width = $container.width();
-				var height = $container.height();
+				const width = $container.width();
+				const height = $container.height();
 
-				var canvasWidth = width * options.pixelRatio;
-				var canvasHeight = height * options.pixelRatio;
+				const canvasWidth = width * options.pixelRatio;
+				const canvasHeight = height * options.pixelRatio;
 
 				canvas.width = canvasWidth;
 				canvas.height = canvasHeight;
 
-				canvas.style.width = width + 'px';
-				canvas.style.height = height + 'px';
+				canvas.style.width = `${width}px`;
+				canvas.style.height = `${height}px`;
 
 				cy.trigger("cyCanvas.resize");
 			}
 
-			cy.on('resize', function() {
+			cy.on("resize", () => {
 				resize();
 			});
 
-			canvas.setAttribute('style', 'position:absolute; top:0; left:0; z-index:' + options.zIndex + ';');
+			canvas.setAttribute("style", `position:absolute; top:0; left:0; z-index:${options.zIndex};`);
 			resize();
 
 			return {
-				ctx: ctx,
+				ctx,
 				// Clear the canvas
-				clear: function() {
-					var width = cy.width();
-					var height = cy.height();
+				clear() {
+					const width = cy.width();
+					const height = cy.height();
 					ctx.save();
 					ctx.setTransform(1, 0, 0, 1, 0, 0);
 					ctx.clearRect(0, 0, width * options.pixelRatio, height * options.pixelRatio);
 					ctx.restore();
 				},
 				// Reset the context transform to an identity matrix
-				resetTransform: function() {
+				resetTransform() {
 					ctx.setTransform(1, 0, 0, 1, 0, 0);
 				},
 				// Set the context transform to match Cystoscape's zoom & pan
-				setTransform: function() {
-					var pan = cy.pan();
-					var zoom = cy.zoom();
+				setTransform() {
+					const pan = cy.pan();
+					const zoom = cy.zoom();
 					ctx.setTransform(1, 0, 0, 1, 0, 0);
 					ctx.translate(pan.x * options.pixelRatio, pan.y * options.pixelRatio);
 					ctx.scale(zoom * options.pixelRatio, zoom * options.pixelRatio);
@@ -77,23 +77,20 @@
 			};
 		};
 
-		cytoscape('core', 'cyCanvas', cyCanvas);
+		cytoscape("core", "cyCanvas", cyCanvas);
 	};
 
-	if (typeof module !== 'undefined' && module.exports) { // expose as a commonjs module
-		module.exports = function(cytoscape) {
+	if (typeof module !== "undefined" && module.exports) { // expose as a commonjs module
+		module.exports = function (cytoscape) {
 			register(cytoscape);
 		};
 	}
 
-	if (typeof define !== 'undefined' && define.amd) { // expose as an amd/requirejs module
-		define('cytoscape-canvas', function() {
-			return register;
-		});
+	if (typeof define !== "undefined" && define.amd) { // expose as an amd/requirejs module
+		define("cytoscape-canvas", () => register);
 	}
 
-	if (typeof cytoscape !== 'undefined') { // expose to global cytoscape (i.e. window.cytoscape)
+	if (typeof cytoscape !== "undefined") { // expose to global cytoscape (i.e. window.cytoscape)
 		register(cytoscape);
 	}
-
-})();
+}());
